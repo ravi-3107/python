cd9337fa2668eefa1d6330255eedffd3a3f540fe
ravi-3107 <ravikumar31072007@gmail.com>
2025-10-16 08:54:35 +0530

    Add files via upload

---
+++ python_1 loop.py
@@ -0,0 +1,32 @@
+# for loop
+
+ #for i in range(n):
+ #    print(i)
+ 
+n=int(input("enter the no of times to repeat"))
+for i in range (n):
+ if i%2==0:
+   print("0",end="")
+ else:
+   print("*",end="")
+
+print("")
+
+
+#horizontal ke liye print me ,end="" add hoga
+
+#while 
+
+# i=int(input(""))
+# n=int(input(""))
+# while i<n:
+#   print("*")
+#   i+=1
+#horizontal
+# i=int(input(""))
+# n=int(input(""))
+# while i<n:
+#   print("*",end="")
+#   i+=1
+
+#Jump statement
